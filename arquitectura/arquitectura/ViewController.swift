577d853a533b08ae3ca14cdb7aa9fdc1552918e0
Jeisson Gonzalez <jsn8802@gmail.com>
2019-08-26 19:55:04 -0500

    comienzo el proyecto

---
+++ arquitectura/arquitectura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arquitectura
+//
+//  Created by Jeisson Gonzalez on 8/26/19.
+//  Copyright © 2019 hidesoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
